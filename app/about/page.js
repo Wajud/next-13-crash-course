52117ba188069cd664bfddb78a507aa5c39d1639
Wajud <wajudkareem757@gmail.com>
2024-04-17 10:24:22 +0100

    end of Header Component

---
+++ app/about/page.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const metadata = {
+  title: "About Traversy Media",
+};
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Traversy Media</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto itaque
+        expedita, tempore minus omnis repellat quisquam incidunt necessitatibus
+        temporibus vero amet iste, praesentium impedit autem. Assumenda quisquam
+        alias libero magni!
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ app/about/team/page.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Our Team</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta ullam
+        minus commodi aspernatur ipsum tenetur in fugiat, soluta impedit
+        aperiam.
+      </p>
+    </div>
+  );
+};
+
+export default TeamPage;
